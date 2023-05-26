b861241dbae05f2f76758f51e27be3f87e00309d
christiangarduno <92418008+christiangarduno@users.noreply.github.com>
2023-05-26 12:56:27 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,13 @@
 import streamlit
+'Would you like to make a Movie or TV Show?'
+movie = st.checkbox('Movie')
+show = st.checkbox('Show')
+
+if movie:
+    option = st.selectbox(
+    'Select the Genre of your Movie',
+    ('Comedy', 'Romantic', 'Action'))
+if show:
+    option = st.selectbox(
+    'Select the Genre of your Show',
+    ('Comedy', 'Romantic', 'Action'))
